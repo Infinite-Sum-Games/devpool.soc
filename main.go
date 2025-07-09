6d07b23822e785e12d93d05356d32fbae1ddd012
Ritesh Koushik <riteshkoushik39@gmail.com>
2025-07-09 18:32:55 +0530

    fix: Multiplexer for bot commands to control gh-ratelimits

--- main.go
+++ main.go
@@ -3,14 +3,13 @@ package main
 import (
 	"fmt"
 
-	"github.com/google/go-github/v62/github"
 	"github.com/redis/go-redis/v9"
 )
 
 var (
 	App    *AppConfig
 	Valkey *redis.Client
-	gh     *github.Client
+	bs     *BotMux
 )
 
 func main() {
---
+++ mux.go
@@ -0,0 +1,59 @@
+package main
+
+import "sync"
+
+type BotMux struct {
+	IssueClaim  chan IssueAction
+	Bounty      chan BountyAction
+	Achievement chan Achievement
+	Solution    chan Solution
+	Mutex       sync.Mutex
+}
+
+func NewBotMux(bufferSize int) *BotMux {
+	return &BotMux{
+		IssueClaim:  make(chan IssueAction, bufferSize),
+		Bounty:      make(chan BountyAction, bufferSize),
+		Achievement: make(chan Achievement, bufferSize),
+		Solution:    make(chan Solution, bufferSize),
+		Mutex:       sync.Mutex{},
+	}
+}
+
+// Initiates the bot multiplexer to list to incoming messages for various event
+// streams and process them accordingly
+func (bm *BotMux) Start() {
+	for {
+		select {
+		case issue := <-bm.IssueClaim:
+			bm.Mutex.Lock()
+			if issue.Extend == true {
+				ManageExtension(issue.ParticipantUsername, issue.Url)
+				bm.Mutex.Unlock()
+				continue
+			}
+			ManageIssueClaim(issue.ParticipantUsername, issue.Url, issue.Claim)
+			bm.Mutex.Unlock()
+		case cash := <-bm.Bounty:
+			bm.Mutex.Lock()
+			ManageBounty(cash.ParticipantUsername, cash.Amount, cash.Action, cash.Url)
+			bm.Mutex.Unlock()
+		case badge := <-bm.Achievement:
+			bm.Mutex.Lock()
+			ManageBadge(badge.ParticipantUsername, badge.Type, badge.Url)
+			bm.Mutex.Unlock()
+		case sol := <-bm.Solution:
+			bm.Mutex.Lock()
+			ManageSolution(sol.ParticipantUsername, sol.Merged, sol.Url)
+			bm.Mutex.Unlock()
+		}
+	}
+}
+
+// Gracefully shuts down the Bot multiplexer
+func (bm *BotMux) Shutdown() {
+	close(bm.IssueClaim)
+	close(bm.Bounty)
+	close(bm.Achievement)
+	close(bm.Solution)
+}
