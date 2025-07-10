356c98b190a0f63b223382c062fda2994f142631
Ritesh Koushik <riteshkoushik39@gmail.com>
2025-07-10 11:00:03 +0530

    add: Validation for environment variables

--- config.go
+++ config.go
@@ -1,22 +1,52 @@
 package main
 
+import (
+	v "github.com/go-ozzo/ozzo-validation/v4"
+	"github.com/spf13/viper"
+)
+
 type AppConfig struct {
 	AppID          int64
 	PrivateKeyPath string
 	Environment    string
-	Repos          []RepoConfig
+	RedisHost      string
+	RedisPort      int
+	RedisUsername  string
+	RedisPassword  string
 }
 
-func NewAppConfig() *AppConfig {
-	return &AppConfig{}
+func NewAppConfig() (*AppConfig, error) {
+	viper.SetConfigFile(".env")
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, err
+	}
+
+	config := &AppConfig{
+		AppID:          viper.GetInt64("APP_ID"),
+		PrivateKeyPath: viper.GetString("PRIVATE_KEY_PATH"),
+		Environment:    viper.GetString("ENVIRONMENT"),
+		RedisHost:      viper.GetString("REDIS_HOST"),
+		RedisPort:      viper.GetInt("REDIS_PORT"),
+		RedisUsername:  viper.GetString("REDIS_USERNAME"),
+		RedisPassword:  viper.GetString("REDIS_PASSWORD"),
+	}
+
+	if err := config.Validate(); err != nil {
+		return nil, err
+	}
+	return config, nil
 }
 
-type RepoConfig struct {
-	InstallationID int64
-	Owner          string
-	Repo           string
-}
-
-func NewRepoConfig() *RepoConfig {
-	return &RepoConfig{}
+func (c *AppConfig) Validate() error {
+	return v.ValidateStruct(c,
+		v.Field(&c.AppID, v.Required),
+		v.Field(&c.PrivateKeyPath, v.Required),
+		v.Field(&c.Environment, v.Required),
+		v.Field(&c.RedisHost, v.Required),
+		v.Field(&c.RedisPort, v.Required, v.Min(1), v.Max(65535)),
+		v.Field(&c.RedisUsername, v.Required),
+		v.Field(&c.RedisPassword, v.Required),
+	)
 }
