2bdcefa7db797216be55e2973fe30b110bc8f999
Ritesh Koushik <riteshkoushik39@gmail.com>
2025-07-10 10:08:25 +0530

    fix: Helper and Testing comment and postComment() function

--- comments.go
+++ comments.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"context"
+
 	"github.com/google/go-github/v62/github"
+	"golang.org/x/oauth2"
 )
 
 const (
-
-	// For managing all issues
 	IssueOnboard = `Howdy contributors, listen up! This issue is under the 
 	watchful gaze of DevPool (official bot of Summer of Code, 2025). Wanna tackle
 	this one ? Just shout "/assign" in the comments and you can fork the repository
@@ -34,7 +34,14 @@ const (
 	BugReport = `Well, well, @%s been paying attention! A bug, you say? 
 	Is it squishy? Does it glow? Anyway, good job finding it! Bug report accepted!`
 
-	// For managing pull requests
+	Tester = `Alright, who's the genius dropping test cases into our code? You @%s! 
+	Nice! Seriously, thanks for making sure this whole thing doesn't blow up. 
+	You're the real MVP, you magnificent creature.`
+
+	Helper = `Hot damn @%s! Someone's got their Good Samaritan pants on today! You 
+	actually went out of your way to lend a hand, and for that, you get a gold star 
+	(and maybe a new shiny badge, if you're lucky). Thanks for being so helpful!`
+
 	PROpened = `Hey @%s! Thank you for opening a pull request. Make sure to tag the 
 	maintainers and link the issue you are solving in your pull-request description 
 	if you have not already and they'll review as soon as possible.`
@@ -43,23 +50,39 @@ const (
 	wheels and cogs to compute your scores. Make sure to check your profile for 
 	any new achievements at [amsoc.vercel.app](https://amsoc.vercel.app/profile).`
 
-	// Bounties and penalties
 	BountyDelivered = `Another day, another coin. Way to get that bounty @%s you 
 	glorious keyboard-tapping, coffee-sipping, vibe-coding witcher!`
 
 	PenaltyDelivered = `Looks like someone took an L today. Chin up, buttercup! 
 	There is always a next time to, NOT get a penalty :')`
-
-	// Unauthorized comments (participants trying to run maintainer bot-commands)
-	PretendMaintainer = `Buddy, that's a maintainers ONLY command! Your 'access' is 
-	as real as my long-term relationship goals. Which is completely non-existant :)
-	Bless your heart for trying LOL!`
-
-	UnregisteredParticipant = `Oopsie @%s! Someone forgot to register for the 
-	Amrita Summer of Code, 2025 program aye ? Register then login via GitHub first.
-	Head over to the [website](https://amsoc.vercel.app/register).`
 )
 
-func PostComment(ctx context.Context, client *github.Client, owner, repo string, issueNumber int, comment string) error {
+func postComment(ctx context.Context, installationToken, owner, repo string,
+	number int, cmt string, sink string) error {
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: installationToken},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+	client := github.NewClient(tc)
 
+	if sink == "issue" {
+		comment := &github.IssueComment{
+			Body: github.String(cmt),
+		}
+		_, _, err := client.Issues.CreateComment(ctx, owner, repo, number, comment)
+		if err != nil {
+			return err
+		}
+	} else {
+		// This handles pull request comments
+		comment := &github.PullRequestComment{
+			Body: github.String(cmt),
+		}
+
+		_, _, err := client.PullRequests.CreateComment(ctx, owner, repo, number, comment)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
